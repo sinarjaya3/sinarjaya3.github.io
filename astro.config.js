c0b6c084a40c2df52e3229b11d6b23b3f8d9a192
mzaini30 <muhzaini30@gmail.com>
2023-08-02 06:31:17 +0800

    Laptop baru

--- astro.config.js
+++ astro.config.js
@@ -1,23 +1,34 @@
 // astro.config.ts
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import yaml from 'vite-plugin-yaml2';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-import { presetTypography, presetUno } from 'unocss';
+import yaml from "vite-plugin-yaml2";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import { presetTypography, presetUno } from "unocss";
+import presetIcons from "@unocss/preset-icons";
 import svelte from "@astrojs/svelte";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sinarjaya3.github.io',
-  integrations: [UnoCSS({
-    injectReset: true,
-    // or a path to the reset file
-    transformers: [transformerVariantGroup()],
-    presets: [presetTypography(), presetUno()]
-  }), svelte(), sitemap()],
+  site: "https://sinarjaya3.github.io",
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+      // or a path to the reset file
+      transformers: [transformerVariantGroup()],
+      presets: [
+        presetTypography(),
+        presetUno(),
+        presetIcons({
+          /* options */
+        }),
+      ],
+    }),
+    svelte(),
+    sitemap(),
+  ],
   vite: {
-    plugins: [yaml()]
-  }
+    plugins: [yaml()],
+  },
 });
